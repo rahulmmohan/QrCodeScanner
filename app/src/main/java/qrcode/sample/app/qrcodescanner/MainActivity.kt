6e13dd0b0a3dd2f3f2c50b4c88a1e1deb935418e
Rahul Mohan <rahul.mohan@CS-113-LT-MAC.local>
2018-04-19 14:20:04 +0530

    Initial commit

---
+++ app/src/main/java/qrcode/sample/app/qrcodescanner/MainActivity.kt
@@ -0,0 +1,61 @@
+package qrcode.sample.app.qrcodescanner
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import com.budiyev.android.codescanner.AutoFocusMode
+import com.budiyev.android.codescanner.CodeScanner
+import com.budiyev.android.codescanner.CodeScannerView
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var mCodeScanner: CodeScanner
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val scannerView = findViewById<CodeScannerView>(R.id.scanner_view)
+        // Use builder
+        mCodeScanner = CodeScanner.builder()
+                /*camera can be specified by calling .camera(cameraId),
+                first back-facing camera on the device by default*/
+                /*code formats*/
+                .formats(CodeScanner.ALL_FORMATS)/*List<BarcodeFormat>*/
+                /*or .formats(BarcodeFormat.QR_CODE, BarcodeFormat.DATA_MATRIX, ...)*/
+                /*or .format(BarcodeFormat.QR_CODE) - only one format*/
+                /*auto focus*/
+                .autoFocus(true).autoFocusMode(AutoFocusMode.SAFE).autoFocusInterval(2000L)
+                /*flash*/
+                .flash(false)
+                /*decode callback*/
+                .onDecoded { result ->
+                    runOnUiThread {
+                        Toast.makeText(this@MainActivity, result.getText(),
+                                Toast.LENGTH_LONG).show()
+                    }
+                }
+                /*error callback*/
+                .onError { error ->
+                    runOnUiThread {
+                        Toast.makeText(this@MainActivity, error.message,
+                                Toast.LENGTH_LONG).show()
+                    }
+                }.build(this, scannerView)
+        // Or use constructor to create scanner with default parameters
+        // All parameters can be changed after scanner created
+        // mCodeScanner = new CodeScanner(this, scannerView);
+        // mCodeScanner.setDecodeCallback(...);
+        scannerView.setOnClickListener {
+            mCodeScanner.startPreview()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mCodeScanner.startPreview()
+    }
+
+    override fun onPause() {
+        mCodeScanner.releaseResources()
+        super.onPause()
+    }
+}
